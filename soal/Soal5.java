7c7324bcedb2487594025d14cd732920597c62a0
LollaM <69521386+LollaM@users.noreply.github.com>
2022-02-13 15:20:08 +0700

    Add files via upload

---
+++ soal/Soal5.java
@@ -0,0 +1,19 @@
+package soal;
+
+class Soal5
+{
+	static short methodOne(long l)
+    {
+        int i = (int) l;
+        return (short)i;
+    }
+    public static void main(String[] args)
+    {
+        double d = 10.25;
+        System.out.println("Nilai d : " + d);
+        float f = (float) d;
+        byte b = (byte) methodOne((long) f);
+        System.out.println("Nilai b : " + b);
+    }
+}
+
